d0dcc6660a69f8e70748f3dbe0aca2a12421ae0f
Jean <jean118877@hotmail.com>
2022-09-08 16:43:36 -0300

    exercício concluido

---
+++ script.js
@@ -0,0 +1,15 @@
+const prompt = require('prompt-sync')();
+
+const userName = prompt('Type your name: ');
+const lastName = prompt('Type your last name: ');
+const weight = prompt('Type your weight: ')
+const height = prompt('Type your height: ')
+const data = prompt('type your date of hospitalization: ')
+const diabetic = prompt('your is diabetic? ')
+
+console.log(`\nYour name: ${userName}`);
+console.log(`Your last name: ${lastName}`);
+console.log(`Your weight: ${weight}`)
+console.log(`Your height: ${height}`)
+console.log(`Date of hospitalization: ${data}`)
+console.log(`Your is diabetic: ${diabetic}`)
